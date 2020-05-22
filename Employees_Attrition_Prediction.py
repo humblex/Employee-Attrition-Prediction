2003c77b227fe744ad33ca05d4c8199af481fd9e
humblex <agwuajahchukwu@gmail.com>
2020-05-22 16:02:12 +0100

    parse the sheets in the Excel file as dataframes, into some variables

--- Employees_Attrition_Prediction.py
+++ Employees_Attrition_Prediction.py
@@ -3,3 +3,7 @@ import pandas as pd
 import numpy as np
 
 AttritionData = pd.ExcelFile("Company_X_Employees_Data.xlsx")
+
+#now parse the sheets as dataframes into a variable
+Employees_Left = AttritionData.parse("Employees who have left")
+Employees_Existing = AttritionData.parse("Existing employees")
